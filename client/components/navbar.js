def798c73a9741a0b41f270457bfd5d6f8ad208f
Christopher Trinh <Chris.trinh@live.com>
2020-09-23 15:25:28 -0400

    feat: Added a single product view

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,6 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
     <nav>
       {isLoggedIn ? (
         <div>
@@ -21,6 +20,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
+          <Link to="/single">Single</Link>
         </div>
       )}
     </nav>
