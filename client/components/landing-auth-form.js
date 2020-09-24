c5f821a3505fca9c615a6139a60f2f38b42eb8eb
Christopher Moon <61075793+supercow112@users.noreply.github.com>
2020-09-24 07:37:19 -0700

    ADD: Animated Landing Page

--- client/components/landing-auth-form.js
+++ client/components/landing-auth-form.js
@@ -1,40 +1,41 @@
-import React from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 /**
  * COMPONENT
  */
 const FormStyling = {
-  width: '100%',
-  margin: 'auto',
-  display: 'flex',
-  flexFlow: 'column',
+  width: "100%",
+  margin: "auto",
+  display: "flex",
+  flexFlow: "column",
   border: 0,
   focus: {
-    backgroundColor: 'lightblue'
+    backgroundColor: "lightblue"
   }
-}
+};
 
 const LandingAuthForm = props => {
   return (
-    <Form onSubmit={props.handleSubmit} style={FormStyling}>
-      <Form.Row>
-        <Form.Group>
+    <Form onSubmit={props.handleSubmit} style={{ margin: "auto" }}>
+      <Form.Row style={{ margin: 0 }}>
+        <Form.Group style={{ width: "60%", margin: 0 }}>
           <Form.Control
             required={true}
             name="email"
             placeholder="Email Address"
+            size="lg"
           />
         </Form.Group>
       </Form.Row>
 
-      <Form.Row>
-        <Form.Group style={{width: '100%'}}>
+      <Form.Row style={{ margin: 0 }}>
+        <Form.Group style={{ width: "60%", margin: 0 }}>
           <Form.Control
             type="url"
             name="password"
             placeholder="Password"
-            width="100%"
+            size="lg"
           />
         </Form.Group>
       </Form.Row>
@@ -43,13 +44,13 @@ const LandingAuthForm = props => {
         <Button
           variant="primary"
           type="submit"
-          style={{width: '100%', margin: '1rem'}}
+          style={{ width: "100%", margin: "1rem" }}
         >
           Log In
         </Button>
       </Form.Row>
     </Form>
-  )
-}
+  );
+};
 
-export default LandingAuthForm
+export default LandingAuthForm;
