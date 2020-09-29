e71e8ba40249b694e14a086aa0ac1c4f6e645225
Kevin Forhan <kevin4forhan@gmail.com>
2020-09-29 15:21:42 -0500

    feat: add form validation

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -5,23 +5,57 @@ import { connect } from "react-redux";
 // import { fetchCartItems } from "../store/cart";
 import { fetchCheckout } from "../store/order";
 import { CheckoutCard } from "./CheckoutCard";
+import { purchaseCart } from "../store/cart";
+import months from "../../server/db/months";
+import years from "../../server/db/years";
 
 class Checkout extends Component {
   constructor() {
     super();
+    this.state = {
+      name: "",
+      cc_number: "",
+      cc_month: "",
+      cc_year: "",
+      cc_cvc: "",
+      billing_street1: "",
+      billing_street2: "",
+      billing_city: "",
+      billing_state: "",
+      billing_zc: "",
+      billing_country: "",
+      shipping_fn: "",
+      shipping_street1: "",
+      shipping_street2: "",
+      shipping_city: "",
+      shipping_state: "",
+      shipping_zc: "",
+      shipping_country: ""
+    };
     this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
   componentDidMount() {
     this.props.fetchCheckout();
   }
-
-  onSubmit() {
-    this.props.history.push("/thankyou");
+  onChange(evt) {
+    console.log("this has fired");
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
   }
 
+  onSubmit() {
+    // this.props.purchaseCart({ billing, shipping });
+    // this.props.history.push("/thankyou");
+  }
   render() {
+    console.log("these are months", months);
     const { order } = this.props;
-
+    const monthArray = Object.keys(months);
+    // console.log("these are months", monthArray)
+    // console.log("these are months", months)
+    console.log("this is the state", this.state);
     const grandTotal = order.reduce((accum, currentValue) => {
       accum += currentValue.quantity * currentValue.product.price;
       return accum;
@@ -41,53 +75,136 @@ class Checkout extends Component {
             <div className="checkout__top__billing">
               <div className="checkout__billing__info">
                 <label>Full Name: </label>
-                <input type="text" placeholder="Full Name" />
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.onChange}
+                />
               </div>
               <div className="checkout__billing__info">
                 <label>Credit Card Number: </label>
-                <input type="text" placeholder="Credit Card Number" />
+                <input
+                  type="text"
+                  placeholder="Credit Card Number"
+                  name="cc_number"
+                  value={this.state.cc_number}
+                  onChange={this.onChange}
+                  maxLength="12"
+                />
               </div>
               <div className="checkout__billing__container">
                 <div className="checkout__billing__info">
                   <label>Month</label>
-                  <input type="text" placeholder="Month" />
+                  {/* <input
+                    type="text"
+                    placeholder="Month"
+                    name="cc_month"
+                    value={this.state.cc_month}
+                    onChange={this.onChange}
+                    maxLength="2"
+
+                  /> */}
+                  <select>
+                    {(() => {
+                      const monthOptions = [];
+                      for (let i = 1; i <= 12; i++) {
+                        monthOptions.push(<option value={`${i}`}>{i}</option>);
+                      }
+                      // console.log("these are months options", monthOptions)
+                      return monthOptions;
+                    })()}
+                  </select>
                 </div>
 
                 <div className="checkout__billing__info">
                   <label>Year</label>
-                  <input type="text" placeholder="Year" />
+                  <input
+                    type="text"
+                    placeholder="Year"
+                    name="cc_year"
+                    value={this.state.cc_year}
+                    onChange={this.onChange}
+                    maxLength="2"
+                  />
                 </div>
 
                 <div className="checkout__billing__info">
                   <label>CVC</label>
-                  <input type="text" placeholder="CVC" />
+                  <input
+                    type="text"
+                    placeholder="CVC"
+                    name="cc_cvc"
+                    value={this.state.cc_cvc}
+                    onChange={this.onChange}
+                    maxLength="3"
+                  />
                 </div>
               </div>
             </div>
             <h5>Billing Address</h5>
             <div className="checkout__address">
               <label>Street 1:</label>
-              <input type="text" placeholder="Street 1" />
+              <input
+                type="text"
+                placeholder="Street 1"
+                name="billing_street1"
+                value={this.state.billing_street1}
+                onChange={this.onChange}
+              />
             </div>
             <div className="checkout__address">
               <label>Street 2: </label>
-              <input type="text" placeholder="Street 2" />
+              <input
+                type="text"
+                placeholder="Street 2"
+                name="billing_street2"
+                value={this.state.billing_street2}
+                onChange={this.onChange}
+              />
             </div>
             <div className="checkout__address">
               <label>City: </label>
-              <input type="text" placeholder="City" />
+              <input
+                type="text"
+                placeholder="City"
+                name="billing_city"
+                value={this.state.billing_city}
+                onChange={this.onChange}
+              />
             </div>
             <div className="checkout__address">
               <label>State: </label>
-              <input type="text" placeholder="State" />
+              <input
+                type="text"
+                placeholder="State"
+                name="billing_state"
+                value={this.state.billing_state}
+                onChange={this.onChange}
+                maxLength="2"
+              />
             </div>
             <div className="checkout__address">
               <label>Zip Code: </label>
-              <input type="text" placeholder="Zip Code" />
+              <input
+                type="text"
+                placeholder="Zip Code"
+                name="billing_zc"
+                value={this.state.billing_zc}
+                onChange={this.onChange}
+                maxLength="5"
+              />
             </div>
             <div className="checkout__address">
               <label>Country: </label>
-              <input type="text" placeholder="Country" />
+              <input
+                type="text"
+                placeholder="Country"
+                name="billing_country"
+                value={this.state.billing_country}
+                onChange={this.onChange}
+              />
             </div>
           </div>
           <div className="checkout__top__right">
@@ -99,31 +216,75 @@ class Checkout extends Component {
             <h5>Shipping Address</h5>
             <div className="checkout__address">
               <label>Full Name: </label>
-              <input type="text" placeholder="Name" />
+              <input
+                type="text"
+                placeholder="Name"
+                value={this.state.shipping_fn}
+                onChange={this.onChange}
+                name="shipping_fn"
+              />
             </div>
             <div className="checkout__address">
               <label>Street 1:</label>
-              <input type="text" placeholder="Street 1" />
+              <input
+                type="text"
+                placeholder="Street 1"
+                name="shipping_street1"
+                value={this.state.shipping_street1}
+                onChange={this.onChange}
+              />
             </div>
             <div className="checkout__address">
               <label>Street 2: </label>
-              <input type="text" placeholder="Street 2" />
+              <input
+                type="text"
+                placeholder="Street 2"
+                name="shipping_street2"
+                value={this.state.shipping_street2}
+                onChange={this.onChange}
+              />
             </div>
             <div className="checkout__address">
               <label>City: </label>
-              <input type="text" placeholder="City" />
+              <input
+                type="text"
+                placeholder="City"
+                name="shipping_city"
+                value={this.state.shipping_city}
+                onChange={this.onChange}
+              />
             </div>
             <div className="checkout__address">
               <label>State: </label>
-              <input type="text" placeholder="State" />
+              <input
+                type="text"
+                placeholder="State"
+                name="shipping_state"
+                value={this.state.shipping_state}
+                onChange={this.onChange}
+                maxLength="2"
+              />
             </div>
             <div className="checkout__address">
               <label>Zip Code: </label>
-              <input type="text" placeholder="Zip Code" />
+              <input
+                type="text"
+                placeholder="Zip Code"
+                name="shipping_zc"
+                value={this.state.shipping_zc}
+                onChange={this.onChange}
+                maxLength="5"
+              />
             </div>
             <div className="checkout__address">
               <label>Country: </label>
-              <input type="text" placeholder="Country" />
+              <input
+                type="text"
+                placeholder="Country"
+                name=""
+                value={this.state.shipping_country}
+                onChange={this.onChange}
+              />
             </div>
           </div>
           <div className="checkout__bottom__right">
@@ -132,7 +293,9 @@ class Checkout extends Component {
                 {numDogs} {numDogs === 1 ? "Dog" : "Dogs"}
               </h4>
               <h4>Total: ${grandTotal}</h4>
-              <button type="button">Checkout</button>{" "}
+              <button type="button" onClick={this.onSubmit}>
+                Checkout
+              </button>{" "}
             </div>
           </div>
         </div>
@@ -149,7 +312,8 @@ const mapState = state => {
 };
 
 const mapDispatch = dispatch => ({
-  fetchCheckout: () => dispatch(fetchCheckout())
+  fetchCheckout: () => dispatch(fetchCheckout()),
+  purchaseCart: () => dispatch(purchaseCart())
 });
 
 export default connect(mapState, mapDispatch)(Checkout);
