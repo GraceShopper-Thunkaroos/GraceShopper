ab3cf1e3fb3755219af5f8bda8c91812b6c1effc
Christopher Moon <61075793+supercow112@users.noreply.github.com>
2020-09-29 20:06:57 -0700

    update: secrets

--- secrets.js
+++ secrets.js
@@ -1,3 +1,4 @@
+// these are the secrets...shh
 process.env.GOOGLE_CLIENT_ID =
   '238981536810-6iclssnuhlicp094tc643kdgi5jtk44n.apps.googleusercontent.com'
 process.env.GOOGLE_CLIENT_SECRET = 'XyptgAJPR7VkfkZ-zimp5CzC'
